b537330ccf118fc29b171328a4cf1e5a43864df7
Mr-warriyo <akshanshsri1234@gmail.com>
2022-01-04 15:27:49 +0530

    Deleted an useless file: SlashCommands/battle-games/BattleFuncs/win-loseFunc.js

--- SlashCommands/battle-games/BattleFuncs/battleStartFuncs.js
+++ SlashCommands/battle-games/BattleFuncs/battleStartFuncs.js
@@ -3,16 +3,11 @@ const client = require("../../../Rem")
 const AttackArr = require("../battlesettings")
 const UserSpecs = require("../battlesettings")
 const BotSpecs = require("../battlesettings")
-
-const botWon = require("./win-loseFunc")
-const userWon = require("./win-loseFunc")
-
 let Player1HP = BotSpecs.hp
 let Player2HP = UserSpecs.hp
 let newBHP
 let newUHP
 let botAttack
-
 function BotAttack() {
   botAttack =
     BotSpecs.attacks[Math.floor(Math.random() * BotSpecs.attacks.length)]
@@ -29,6 +24,76 @@ let randomAttack
 function RandomAttack() {
   randomAttack =
     UserSpecs.attacks[Math.floor(Math.random() * UserSpecs.attacks.length)]
+}
+const battleModel = require("../../../models/battleModel")
+async function userWon(botId, userId, msg) {
+  const uM = await battleModel.findOne({
+    userId,
+  })
+
+  if (uM) {
+    const wins = uM.wins + 1
+    const loses = uM.loses
+    const totalGamesPlayed = uM.totalGamesPlayed + 1
+    const newUM = await battleModel.findOneAndUpdate(
+      {
+        userId,
+      },
+      {
+        wins,
+        loses,
+        totalGamesPlayed,
+      },
+      {
+        new: true,
+      }
+    )
+  } else {
+    const wins = 1
+    const loses = 0
+    const totalGamesPlayed = 1
+    const newUM = await battleModel.create({
+      userId,
+      wins,
+      loses,
+      totalGamesPlayed,
+    })
+  }
+}
+
+async function botWon(botId, userId, msg) {
+  const uM = await battleModel.findOne({
+    userId,
+  })
+
+  if (uM) {
+    const wins = uM.wins
+    const loses = uM.loses + 1
+    const totalGamesPlayed = uM.totalGamesPlayed + 1
+    const newUM = await battleModel.findOneAndUpdate(
+      {
+        userId,
+      },
+      {
+        wins,
+        loses,
+        totalGamesPlayed,
+      },
+      {
+        new: true,
+      }
+    )
+  } else {
+    const wins = 0
+    const loses = 1
+    const totalGamesPlayed = 1
+    const newUM = await battleModel.create({
+      userId,
+      wins,
+      loses,
+      totalGamesPlayed,
+    })
+  }
 }
 
 async function start(interaction, BattleBeginEm) {
