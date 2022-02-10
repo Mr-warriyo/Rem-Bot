db8f0237fca7c544ae736b42be5c1ba001c4ccc1
Mr-warriyo <akshanshsri123@gmail.com>
2022-02-10 18:57:53 +0530

    Minor Changes

--- events/music.js
+++
@@ -1,7 +0,0 @@
-const { event } = require("/data/data/com.termux/files/usr/lib/node_modules/op-music")
-
-event.on("playSong", (channel, songInfo, requester) => {
-  channel.send({
-    content: `Started playing the song [${songInfo.title}](${songInfo.url}) - ${songInfo.duration} | Requested by \`${requester.tag}\``,
-  })
-})
